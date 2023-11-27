8b02e58858e86fdb5352e952b90cafe46ed0c4de
Samuel Danquah <samueldanquah@Samuels-MacBook-Pro.local>
2023-11-27 16:24:35 +0000

    Adds services page

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,3 +8,22 @@ hamburger.addEventListener("click", () => {
     mobileNav.classList.add("hidden");
   }
 });
+
+const topBtn = document.getElementById("top");
+
+const scrollFunction = () => {
+  // console.log("window scrolled!");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "flex";
+  } else {
+    topBtn.style.display = "none";
+  }
+};
+
+const topFunction = () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+window.addEventListener("scroll", scrollFunction);
+topBtn.addEventListener("click", topFunction);
